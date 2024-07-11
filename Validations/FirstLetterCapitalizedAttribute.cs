5e73b2d471f11ab49c62e544de99dbabf3ff9706
DevBruno <brunoantonio.dev0016@gmail.com>
2024-07-11 16:06:31 -0300

    Criação da pasta Validations, que vai conter as classes referentes a validação e criação da primeira classe de validação

---
+++ Validations/FirstLetterCapitalizedAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APICatalog.Validations
+{
+    public class FirstLetterCapitalizedAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            var firstLetter = value.ToString()[0].ToString();
+            if (firstLetter != firstLetter.ToUpper())
+            {
+                return new ValidationResult("A primeira letra do nome do produto deve ser maiúscula");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
